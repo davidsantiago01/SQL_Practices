2fdbe62eb480ddf1f3ce8c3a5ac38379c457c9a3
David Santiago <56009621+davidsantiago01@users.noreply.github.com>
2025-08-31 00:14:40 -0600

    Create Bottom_employees.sql

---
+++ Bottom_employees.sql
@@ -0,0 +1,11 @@
+SELECT LastName, FirstName,
+(SELECT SUM(od.Quantity) from [orders] o, [OrderDetails] od
+where o.EmployeeID = e.EmployeeID and od.OrderID = o.OrderID) as unidades_totales
+from [Employees] e
+where unidades_totales < (SELECT avg(unidades_totales) from (
+select (SELECT SUM(od.Quantity) from [orders] o, [OrderDetails] od
+where o.EmployeeID = e2.EmployeeID and od.OrderID = o.OrderID)as unidades_totales 
+from [Employees] e2
+group by e2.EmployeeID	
+
+))
